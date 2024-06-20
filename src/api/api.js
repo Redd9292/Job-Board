ad968f44be666a4eddb8089136dcafa8fff5f4de
User Name <redd@Gilbertos-MacBook-Pro.local>
2024-06-20 16:50:17 -0500

    api update

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,24 @@
 const ADZUNA_APP_ID = import.meta.env.VITE_ADZUNA_APP_ID;
 const ADZUNA_APP_KEY = import.meta.env.VITE_ADZUNA_APP_KEY;
+const ADZUNA_API_BASE_URL = "https://api.adzuna.com";
+console.log("ADZUNA_APP_ID:", ADZUNA_APP_ID);
+console.log("ADZUNA_APP_KEY:", ADZUNA_APP_KEY);
 
 export const fetchJobs = async (description = "", location = "") => {
-  const response = await fetch(
-    `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${ADZUNA_APP_ID}&app_key=${ADZUNA_APP_KEY}&what=${description}&where=${location}`
-  );
-  const data = await response.json();
-  return data.results;
+  const url = `${ADZUNA_API_BASE_URL}/v1/api/jobs/us/search/1?app_id=${ADZUNA_APP_ID}&app_key=${ADZUNA_APP_KEY}&what=${description}&where=${location}`;
+  console.log("Fetching jobs from URL:", url);
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      console.error(`Error fetching job details: ${response.statusText}`);
+      throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
+    const data = await response.json();
+    console.log("API Response Data:", data);
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching job details:", error);
+    throw error;
+  }
 };
